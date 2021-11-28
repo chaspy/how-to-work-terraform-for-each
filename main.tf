30a500fb80cccb42b3068163153b84295579dc91
kondo takeshi <chaspy@users.noreply.github.com>
2021-11-28 17:52:03 +0900

    Use sample
    https://qiita.com/minamijoyo/items/3785cad0283e4eb5a188

--- main.tf
+++ main.tf
@@ -1,62 +1,22 @@
-resource "kubernetes_namespace" "redash" {
-  metadata {
-    name = local.redash.namespace
-  }
-}
-
-resource "random_password" "redash_cookie_secret" {
-  count = length(local.redash_data)
-
-  length  = 32
-  special = false
-}
-
-resource "random_password" "redash_secret_key" {
-  count = length(local.redash_data)
-
-  length  = 32
-  special = false
-}
-
-resource "helm_release" "redash" {
-  count = length(local.redash_data)
-
-  name       = local.redash_data[count.index].name
-  namespace  = local.redash.namespace
-  repository = local.redash.chart_repository
-  chart      = local.redash.chart_name
-  version    = local.redash.chart_version
-
-  values = [file("values.redash.yml")]
-
-  dynamic "set" {
-    for_each = local.redash_data[count.index].values
-    content {
-      name  = set.value.name
-      value = set.value.value
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
     }
   }
+}
 
-  dynamic "set_sensitive" {
-
-    for_each = toset(concat([
-      {
-        name  = "redash.secretKey"
-        value = base64encode(random_password.redash_secret_key[count.index].result)
-      },
-      {
-        name  = "redash.cookieSecret"
-        value = base64encode(random_password.redash_cookie_secret[count.index].result)
-      }
-    ], local.redash_data[count.index].secret_values))
-
-    content {
-      name  = set_sensitive.value.name
-      value = set_sensitive.value.value
-    }
-  }
-  timeout = 600
-  depends_on = [
-    kubernetes_namespace.redash,
+locals {
+  images = [
+    { name = "foo", image = "alpine" },
+    { name = "bar", image = "debian" },
   ]
+}
+
+resource "docker_container" "this" {
+  # for_each = local.images ここにmapのlistを渡したいがエラーになる
+  for_each = { for i in local.images : i.name => i } # こう書くのが正しい
+  name     = each.value.name
+  image    = each.value.image
 }
